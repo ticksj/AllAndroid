204a461d5563ff2b2cf9b0057f08a6bce945348d
tick <1085798032@qq.com>
2019-03-22 23:08:51 +0800

    ConstraintLayout使用

---
+++ b_view/src/main/java/com/sj/b_view/ConstraintLayoutActivity.java
@@ -0,0 +1,14 @@
+package com.sj.b_view;
+
+import android.os.Bundle;
+
+import com.sj.s_base.BaseActivity;
+
+public class ConstraintLayoutActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraint_layout);
+    }
+}
